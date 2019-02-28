4fccddf026e998b52c22b9acf8f202550910542c
hissa <hissa@users.noreply.github.com>
2019-02-28 15:07:15 +0900

    Success to run a server as subprocess.

--- mcsrv/srv/mnt/app.js
+++ mcsrv/srv/mnt/app.js
@@ -1,2 +1,12 @@
-const http = require('http');
 const express = require('express');
+const app = express();
+const childProcess = require('child_process');
+
+var p = childProcess.spawn('java', ['-jar', './server.jar'],{cwd: './mc'});
+p.stdout.on('data', data => console.log(`stdout: ${data}`));
+p.stderr.on('data', data => console.error(`stderr: ${data}`));
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+// app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
