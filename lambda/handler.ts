a9ef1b29fc42d4d7e2c201b8a4dbd625a510b138
hissa <hissamassa7@gmail.com>
2019-03-23 15:42:28 +0900

    Change indent pattern to 4 spaces.

--- lambda/handler.ts
+++ lambda/handler.ts
@@ -1,12 +1,22 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
+import { APIGatewayProxyHandler, CloudWatchLogsHandler } from 'aws-lambda';
+import { McsrvProvider } from './mcsrvProvider';
+import { Server } from './server';
+import { ServerProvider } from './serverProvider';
 
 export const hello: APIGatewayProxyHandler = async (event, _context) => {
-  console.log(process.env);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+    console.log(process.env);
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+            input: event,
+        }),
+    };
 }
+
+export const update: CloudWatchLogsHandler = async (event, _context) => {
+    const mcsrvProvider = new McsrvProvider();
+    const serverProvider = new ServerProvider();
+
+    
+};
